718609b09964f261d4de456b0e92a6ef5f58bdea
archiesinaga <archiesinaga9@gmail.com>
2025-07-08 08:21:24 +0700

    input audio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,23 @@ import React, { use } from 'react'
 import { useGSAP } from '@gsap/react'
 import { SplitText } from 'gsap/all'
 import gsap from 'gsap'
+import { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const Hero = () => {
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({ maxWidth: 767 })
+
   useGSAP(() => {
-    const heroSplit = new SplitText('.title', { type: 'chars, words' });
-    const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
+    const heroSplit = new SplitText('.title', { 
+      type: 'chars, words' 
+    });
+
+    const paragraphSplit = new SplitText('.subtitle', 
+      { type: 'lines', 
+      });
+
     heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
 
     gsap.from(heroSplit.chars, {
@@ -25,7 +37,8 @@ const Hero = () => {
       delay: 1
     })
 
-    gsap.timeline({
+    gsap
+    .timeline({
       scrollTrigger: {
         trigger: '#hero',
         start: 'top top',
@@ -35,6 +48,26 @@ const Hero = () => {
     })
     .to('.right-leaf', { y: 200 }, 0)
     .to('.left-leaf', { y: -200 }, 0)
+    .to(".arrow", { y:100 }, 0);
+
+    const startValue = isMobile? 'top 50%' : 'center 60%';
+    const endValue = isMobile? '120% top' : 'bottom top';
+
+    let t1 = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'video',
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      t1.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
 
   return (
@@ -55,6 +88,8 @@ const Hero = () => {
             />
 
             <div className="body">
+              {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
+              
                 <div className="content">
                     <div className="space-y-5 hidden md:block">
                       <p>Cool. Crisp. Classic.</p>  
@@ -73,6 +108,16 @@ const Hero = () => {
                 </div>
             </div>
         </section>
+
+        <div className="video absolute inset-0">
+          <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+          />
+        </div>
     </>
    
   )
